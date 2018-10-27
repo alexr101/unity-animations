a37a2bf5fcebb952ae714187455a0baf4733b720
alexr101 <alex_r_101@hotmail.com>
2018-10-27 12:03:36 -0400

    add basic wait animations + sync them to inputs

---
+++ Assets/scripts/player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+    public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown("1")) {
+            anim.Play("WAIT01", 0, 0f);
+        }
+        if (Input.GetKeyDown("2"))
+        {
+            anim.Play("WAIT02", 0, 0f);
+        }
+        if (Input.GetKeyDown("3"))
+        {
+            anim.Play("WAIT03", 0, 0f);
+        }
+        if (Input.GetKeyDown("4"))
+        {
+            anim.Play("WAIT04", 0, 0f);
+        }
+	}
+}
